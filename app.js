420b102e460f50cc97f0d522961be1b068ac9e34
sdpalmer79 <76813706+sdpalmer79@users.noreply.github.com>
2022-10-03 11:27:43 +0300

    test commit from branch (#1)

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
     //test
+    //test
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Autorization');
     next();
 });

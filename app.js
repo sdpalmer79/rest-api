d46b28f4137edf9e754d69a29ec9d8b658d7bf6e
sdpalmer79 <76813706+sdpalmer79@users.noreply.github.com>
2022-10-03 11:20:01 +0300

    test commit to master

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    //test
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Autorization');
     next();
 });
